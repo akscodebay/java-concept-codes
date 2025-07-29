36115d2af63583b160f51200c20559213db4130c
aks <dhpdgwmqo98oafdo@gmail.com>
2025-07-29 23:35:26 +0530

    added BinaryTree package

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class Solution {
-    public int candy(int[] ratings) {
-        int len = ratings.length;
-        int minCandyCount = len;
-        int indexPrev=-1;
-        for(int i=1;i<len;i++){
-            System.out.println("i-"+i);
-            if(ratings[i-1]>ratings[i] && indexPrev!=i-1){
-                minCandyCount++;
-                indexPrev=i-1;
-                System.out.println("t1-"+indexPrev);
-            }
-            else if(ratings[i-1]<=ratings[i] && indexPrev!=i-1){
-                minCandyCount++;
-                indexPrev=i;
-                System.out.println("t2-"+indexPrev);
-            }
-        }
-        return minCandyCount;
-    }
+
+
     public static void main(String[] args) {
-		int[] ratings = {1,2,87,87,87,2,1};
-		Solution obj = new Solution();
-		System.out.println(obj.candy(ratings));
-
+        List<String> list = Arrays.asList("one", "three", "five", "six", "seven");
+        Map<String, String> map = list.stream()
+                .collect(Collectors.toMap(i->i, i->i,
+                        (oldVal, newVal)->oldVal));
+        System.out.println(map);
     }
-
-} 
+}
